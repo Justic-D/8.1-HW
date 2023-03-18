3238b1842216dc177512c100d0c99d682c10b172
Justic-D <mmobsfg@yandex.ru>
2023-03-18 23:40:32 +0500

    12.3.4

--- script/2.sql
+++ script/2.sql
@@ -1,5 +1,9 @@
-SELECT payment_id , payment_date , amount
+SELECT 
+payment_id , 
+DATE_FORMAT(payment_date, '%d - %m - %y') AS 'Date' ,
+amount
 FROM payment
-WHERE payment_date BETWEEN  '2005-06-15' AND '2005-06-19' 
+WHERE payment_date
+BETWEEN  '2005-06-15' AND '2005-06-19' 
 AND amount > 10 
 ORDER BY payment_date DESC;
