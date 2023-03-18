d1fc60d096b0218b39a148859b08c098e48629c1
Justic-D <mmobsfg@yandex.ru>
2023-03-18 21:19:26 +0500

    12.3.2

--- script/6.sql
+++ script/6.sql
@@ -1,5 +1,7 @@
-SELECT email  , SUBSTRING_INDEX (email  , '@', 1) , CONCAT ( LEFT(UPPER(SUBSTRING_INDEX(email  , '@', 1)), 1),
-LOWER(SUBSTR((SUBSTRING_INDEX(email , '@',1)),2))) as '1' , SUBSTRING_INDEX(email  , '@', -1) ,
-CONCAT(LEFT(UPPER(SUBSTRING_INDEX(email  , '@', -1)), 1) , LOWER(SUBSTR((SUBSTRING_INDEX(email , '@',-1)),2))) as '2'
+SELECT email  , SUBSTRING_INDEX (email  , '@', 1) , CONCAT (LEFT(UPPER(SUBSTRING_INDEX(email  , '@', 1)), 1),
+LOWER (SUBSTR((SUBSTRING_INDEX(email , '@',1)),2))) as '1' ,
+SUBSTRING_INDEX (email  , '@', -1) ,
+CONCAT (LEFT(UPPER(SUBSTRING_INDEX(email  , '@', -1)), 1) ,
+LOWER (SUBSTR((SUBSTRING_INDEX(email , '@',-1)),2))) as '2'
 FROM customer
 LIMIT 10;
